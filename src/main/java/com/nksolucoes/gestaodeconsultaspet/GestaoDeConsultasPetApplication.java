6f2afdae8844ae878de14fcccc6096b2929903f2
Nicolas Pires <nicolask.pires@gmail.com>
2021-07-26 08:38:38 -0300

    Projeto Criado

---
+++ src/main/java/com/nksolucoes/gestaodeconsultaspet/GestaoDeConsultasPetApplication.java
@@ -0,0 +1,13 @@
+package com.nksolucoes.gestaodeconsultaspet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeConsultasPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeConsultasPetApplication.class, args);
+	}
+
+}
